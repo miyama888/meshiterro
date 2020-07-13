1b3f7d55e74447e09dacee7c404f06c5de67a88c
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-07-13 23:19:04 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '59af6dcf1d3261be664f975021b7cdab1c7f8a27abd1313aae5a78589b8242fa24a29c5b3677f994f2b86789818da06001ea583344fd2f273da96b17f14f085c'
